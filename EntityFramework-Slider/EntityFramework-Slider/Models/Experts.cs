f8e5656b0709f82ad629d70edaac3f62aa052591
unknown <royaam@code.edu.az>
2023-04-24 04:06:26 +0400

    task are ready

---
+++ EntityFramework-Slider/EntityFramework-Slider/Models/Experts.cs
@@ -0,0 +1,11 @@
+﻿namespace EntityFramework_Slider.Models
+{
+    public class Experts:BaseEntity
+    {
+        public string? Image { get; set; }
+
+        public string? Name { get; set; }
+
+        public string? Position { get; set; }
+    }
+}
